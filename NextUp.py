e7add342e49759a5dc07d3c43154e4fb9d80ebcb
Vignesh Rajmohan <vrajmoha@andrew.cmu.edu>
2018-11-11 16:42:16 -0500

    Update NextUp.py

--- NextUp.py
+++ NextUp.py
@@ -1,10 +1,3 @@
-# README: The purpose of this file is to update the local json file to include all the songs in the Spotify playlist,
-# and then reorder the playlist to have the songs with the most votes appear first on the playlist.
-# Note this file does NOT add votes to the json file
-# For this file to work in conjunction with the app you MUST call the file, then add a username ("diegofinni" in our
-# case), followed by an id of the playlist and then the ids of all the tracks you want to add to the playlist
-# Example of calling file on terminal: python3 NextUp.py diegofinni 5K3rtFT1Tq19lJ04wjuVBV 59WN2psjkt1tyaxjspN8fp
-# The client data needed for this file to work (CLIENT ID, SECRET, and REDIRECT_URI) are already given
 
 import sys
 import spotipy
