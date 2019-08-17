81c2c8ad14c39e7813feb323a02b891c7bbc2033
SeanPrendi <sean.prendi@gmail.com>
2019-08-17 01:15:10 -0400

    Converted functions and global variables into an organized and documented object.

--- NextUp.py
+++ NextUp.py
@@ -1,110 +1,116 @@
-
 import sys
 import spotipy
 import spotipy.util as util
-from algor1 import *
+import json
+from private_token import get_token
 
-#######################################################################################################################
-# These functions fetch a json file and then add any song to the json
-# file that is not already on it and gives it a default value of 0
-
-filename = "votes.json"
-
-
-def get_list():
-    with open(filename) as file:
-        return json.load(file)
-
-
-# def update_list(lst):
-#     dic = get_list()
-#     for song in lst:
-#         if song not in dic:
-#             dic[song] = 0
-#
-#     set_list(dic)
-
-def add_new_to_list(song):
-    dic = get_list()
-    song['votes'] = 1
-    dic[song['id']] = song
-    set_list(dic)
+# This object contains the functions for interacting with the votes on the site
+# Input:
+#   - filename (str) - the file to import the json of votes
+class PlaylistManipulator(object):
+    def __init__(self, filename):
+        self.filename = filename
+        self.dict = self.get_list()
+        self.token = get_token()
+        self.spotify = spotipy.Spotify(auth=self.token)
+        self.username = ""
+        self.playlist = ""
+        self.track_ids = [""]
 
 
-def set_list(dic):
-    with open(filename, 'w') as file:
-        return json.dump(dic, file, indent=2)
+    # Change the json file that votes are being loaded from
+    def reset_filename(self, filename):
+        self.filename = filename
 
+    # Retrieve a list of current votes from the json file
+    def get_list(self):
+        with open(self.filename) as file:
+            return json.load(file)
 
-def read_playlist():
-    with open(filename, "r") as file:
-        return json.load(file)
+    # Add a song to the dictionary of songs, starting with a single vote
+    def add_new_to_list(self, song):
+        song['votes'] = 1
+        self.dict[song['id']] = song
+        self.set_list()
 
+    # Update the json file to reflect new votes
+    def set_list(self):
+        with open(self.filename, 'w') as file:
+            return json.dump(self.dict, file, indent=2)
 
-########################################################################################################################
+    # Returns all of the songs sorted by votes
+    def best_songs(self):
+        result = list(self.dict.values())
+        result.sort(key=lambda song: song['votes'])
+        result.reverse()
+        return result
 
-# This function sorts through the playlist to make
-# a list of the ids and a list of the song titles
-def show_tracks(tracks):
-    result = {}
+    # Increments the number of votes for the specified song
+    def vote_for(self, song):
+        self.dict[song]['votes'] += 1
+        self.set_list()
 
-    for i, item in enumerate(tracks['items']):
-        track = item['track']
-        result[track['id']] = {
-            'id': track['id'],
-            'name': track['name'],
-            # 'artist': track['artist'],
-        }
+    def reset_votes(self):  # sets all song votes to zero
+        for song in self.dict:
+            self.dict[song] = 0
+        self.set_list()
 
-    return result
+    # Retrieves the current most popular song from the dictionary
+    def most_popular(self):
+        popularVote = 0
+        popular = None
+        for song in self.dict:
+            if popular == None:  # for first song in the list, sets to best
+                popular = song
+                popularVote = self.dict[song]
+            # if equal votes, returns first song in list (ie higher in queue)
+            if self.dict[
+                song] > popularVote:  # if song has more votes, sets to best
+                popularVote = self.dict[song]
+                popular = song
+        return popular  # returns the string
 
+    # Returns a dictionary of song IDs and names in a given playlist
+    def show_tracks(self, tracks):
+        result = {}
+        for i, item in enumerate(tracks['items']):
+            track = item['track']
+            result[track['id']] = {
+                'id': track['id'],
+                'name': track['name'],
+                # 'artist': track['artist'],
+            }
+        return result
 
-# This function reorders the Spotify playlist
-# based on votes found in the json file
-def reorder_playlist(sp, song_ids):
-    new_order = best_songs()
-    for i in range(len(new_order) -1, -1, -1):
-        for j in range(len(song_ids)):
-            if new_order[i]['id'] == song_ids[j]:
-                song_ids.insert(0, song_ids[j])
-                song_ids.pop(j + 1)
-                sp.user_playlist_reorder_tracks(username, playlist, j, 0, 1)
+    # Takes in a Spotify object and a list of song ids, then sorts the song ids
+    # by the number of current votes
+    def reorder_playlist(self, song_ids):
+        new_order = self.best_songs()
+        for i in range(len(new_order) - 1, -1, -1):
+            for j in range(len(song_ids)):
+                if new_order[i]['id'] == song_ids[j]:
+                    song_ids.insert(0, song_ids[j])
+                    song_ids.pop(j + 1)
+                    self.spotify.user_playlist_reorder_tracks(self.username, self.playlist, j, 0, 1)
 
-
-# This if/else makes sure all the Spotify data (id, secret, uri) are inputted
-# Note that it is assumed you are inputting a username, playlist, and a list of track_ids
-# HACK
-username = ""
-playlist = ""
-track_ids = [""]
-# HACK
-
-scope = 'playlist-modify-public'
-# Example data
-token = util.prompt_for_user_token(username, scope, client_id='171b3cdfebb344ba9772c6859136c2d4',
-                                   client_secret='Fca0bb0ea99f493d987b27c9568b79ee',
-                                   redirect_uri='http://www.google.com/')
-
-# If a valid token is used, this if statement compiles all of the helper
-#  functions to execute the purpose of this file(read README if confused)
-def vote_for_song(song):
-    sp = spotipy.Spotify(auth=token)
-    sp.trace = False
-    results = sp.user_playlist(username, playlist, fields="tracks,next")
-    tracks = results['tracks']
-    songs = show_tracks(tracks)
-
-    if song not in songs:
-        sp.user_playlist_add_tracks(username, playlist, [song])
-
-        results = sp.user_playlist(username, playlist, fields="tracks,next")
+    # Takes in a Spotify object and increments the votes for the specified song
+    def vote_for_song(self, song):
+        # sp = spotipy.Spotify(auth=token)
+        self.spotify.trace = False
+        results = self.spotify.user_playlist(self.username, self.playlist, fields="tracks,next")
         tracks = results['tracks']
-        songs_new = show_tracks(tracks)
+        songs = self.show_tracks(tracks)
 
-        add_new_to_list(songs_new[song])
+        if song not in songs:
+            self.spotify.user_playlist_add_tracks(self.username, self.playlist, [song])
 
-    else:
-        vote_for(song)
+            results = self.spotify.user_playlist(self.username, self.playlist, fields="tracks,next")
+            tracks = results['tracks']
+            songs_new = self.show_tracks(tracks)
 
-    reorder_playlist(sp, list(songs.keys()))
-########################################################################################################################
+            self.add_new_to_list(songs_new[song])
+
+        else:
+            self.vote_for(song)
+
+        self.reorder_playlist(list(songs.keys()))
